0db1fbebb5bfbd469962acc7be1c344e517be9ee
EssenceOfnull <tomaskrul@gmail.com>
2025-10-18 13:28:19 +0300

    Fix for allowed domains

--- Backend/CSSASiteBackend/CSSASiteBackend/settings.py
+++ Backend/CSSASiteBackend/CSSASiteBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9e2w6)#r%6en=n8=a-hwyq$b1)ljp@oe%f#6uo9io9rv(zs1b0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
